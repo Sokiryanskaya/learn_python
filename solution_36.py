a7c02501f0cef9d12008d2c9c1125a4cd4bfcb8d
Elena Sokiryanskaya <sokiryanskaya@gmail.com>
2018-01-04 22:38:21 +0300

    тест

--- solution_36.py
+++ solution_36.py
@@ -1,6 +1,6 @@
 a, b, c=int(input()), int(input()), int(input())
 p = (a + b + c)/2.0 #Полупериметр
-S = (p * (p -a) * (p - b) * (p - c)) ** 0.5
+S = (p * (p -a) * (p - b) * (p - c)) ** 0.6
 
 print(S)
 
